086df161d6c4d5a6d370b700cd389f87f5571f47
Issiah Cantrell <mochodawg@aol.com>
2017-04-03 14:39:23 -0700

    edited perlin noise # Please enter the commit message for your changes. Lines starting

--- perlin_noise/perlin_noise_hyperdimensional.py
+++ perlin_noise/perlin_noise_hyperdimensional.py
@@ -10,16 +10,17 @@ def interpolate(x0,x1,w):
 def get_gradient(coord):
     gradient = []
     for i in xrange(len(coord)):
+        #set seeds for procedural gradients at vertecies.
         random.seed((i+1)*int(coord[i]))
         gradient.append(random.random())
         #use srand for each dimension
         #so that (rand(n),rand(n),...) never occurs.
-    #normalize vector
+        
+    #normalize the vector
     magnitude = 0
     for i in xrange(len(gradient)):
         magnitude += gradient[i]*gradient[i]
     magnitude = math.sqrt(magnitude)
-    
     for i in xrange(len(gradient)):
         gradient[i] = gradient[i]/magnitude
         
@@ -43,24 +44,32 @@ def perlin(coord):
         vec = []
         
         for x in xrange(len(coord)):
+            #floor the coordinate components.
             if coord[x]<0:
                 corners[i].append(float(int(coord[x]) - 1))
             else:
                 corners[i].append(float(int(coord[x])))
-            #add 1 to each
+            #add 1 to each vector's components depending on their index.
+            #i will range through from (0) to (2**(n) - 1) so changing i's binary forms
+            #into a vectors will cover each point on the hypercube of n dimensions.
+            #And we can use 2**x for {x:0 to (n-1)} to add to the individual components.
             if i&2**x:
                 corners[i][x] += 1
+            #get vector of corner to coordinate.
             vec.append(coord[x] - corners[i][x])
-            
+        
+        #get dot prod of vector and it's corresponding gradient.    
         dot_products.append(dot(get_gradient(corners[i]),vec))
     
-    
+    #vector used for interpolation
     interpolate_vec = [coord[i] - corners[0][i] for i in xrange(len(coord))]
     
     interpolated_values = dot_products  
+    #interpolate along each dimension.
     for dim in xrange(len(coord),0,-1):
-        #interpolate along each dimension.
+        #in each dimensions here will be 2**(dim-1) interpolations.
         for i in xrange(2**(dim-1)):
+            #interpolate along axes from largest to smallest. e.g y axis(1) then x axis(0)
             interpolated_values[i] = interpolate(interpolated_values[i], interpolated_values[i + 2**(dim-1)], interpolate_vec[dim-1])
             
     return interpolated_values[0]
